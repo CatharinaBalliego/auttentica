d3bad18a239522910883d4c144fb503afac42a74
Catharina <catharina_@hotmail.com.br>
2024-07-15 15:40:01 -0300

    initial commit

---
+++ src/test/java/com/catharina/auttentica/AuttenticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catharina.auttentica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuttenticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
